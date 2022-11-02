facf4c5417d6c13483ebe6b8b2b12ac30be92d78
Gilberto Gabon <gagabon501@yahoo.com>
2022-11-02 15:53:14 +1300

    Set opening form to start in the middle of the main sreen

--- MyCovidRecord/mycovidrecord.cpp
+++ MyCovidRecord/mycovidrecord.cpp
@@ -1,12 +1,15 @@
 #include "mycovidrecord.h"
 #include "ui_mycovidrecord.h"
 #include "login.h"
+#include <QScreen>
 
 MyCovidRecord::MyCovidRecord(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MyCovidRecord)
 {
     ui->setupUi(this);
+    move(QGuiApplication::screens().at(0)->geometry().center() - frameGeometry().center()); //center the form in the main window
+//    this->showMaximized();
 }
 
 MyCovidRecord::~MyCovidRecord()
