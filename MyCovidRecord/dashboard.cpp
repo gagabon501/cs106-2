064d407c52367d3dcf61b903c746ac13319682d1
Gilberto Gabon <gagabon501@yahoo.com>
2022-11-02 23:56:57 +1300

    Added menu and toolbar in Dashboard screen

--- MyCovidRecord/dashboard.cpp
+++ MyCovidRecord/dashboard.cpp
@@ -1,5 +1,6 @@
 #include "dashboard.h"
 #include "ui_dashboard.h"
+#include <QScreen>
 
 Dashboard::Dashboard(QWidget *parent) :
     QMainWindow(parent),
@@ -7,9 +8,17 @@ Dashboard::Dashboard(QWidget *parent) :
 {
     ui->setupUi(this);
 //    this->showMaximized();
+
+    move(QGuiApplication::screens().at(0)->geometry().center() - frameGeometry().center()); //center the form in the main window
 }
 
 Dashboard::~Dashboard()
 {
     delete ui;
 }
+
+void Dashboard::on_actionQuit_triggered()
+{
+    QApplication::quit();
+}
+
--- MyCovidRecord/dashboard.h
+++ MyCovidRecord/dashboard.h
@@ -15,6 +15,9 @@ public:
     explicit Dashboard(QWidget *parent = nullptr);
     ~Dashboard();
 
+private slots:
+    void on_actionQuit_triggered();
+
 private:
     Ui::Dashboard *ui;
 };
