1cd72c592dfd3b9f3057e91a9bdca6ceb87f31aa
Gilberto Gabon <gagabon501@yahoo.com>
2022-11-03 15:52:37 +1300

    Added hashing of password in Registration module

--- MyCovidRecord/login.cpp
+++ MyCovidRecord/login.cpp
@@ -1,5 +1,8 @@
 #include "login.h"
 #include "ui_login.h"
+#include <QString>
+#include <QCryptographicHash>
+#include <QDebug>
 
 Login::Login(QWidget *parent) :
     QDialog(parent),
@@ -16,6 +19,10 @@ Login::~Login()
 void Login::on_pushButton_clicked()
 {
 
+    QString password = ui->lineEdit_password->text();
+    QByteArray hashed_password = QCryptographicHash::hash(password.toUtf8(),QCryptographicHash::Sha224).toHex();
+    qDebug() << "hashed: " << hashed_password;
+    //searh db, then get the stored hashed password, then compare with the hashed password
     this->close();
     dashboard = new Dashboard();
     dashboard->show();
--- MyCovidRecord/registration.cpp
+++ MyCovidRecord/registration.cpp
@@ -1,6 +1,9 @@
 #include "registration.h"
 #include "ui_registration.h"
 #include <dbmanager.h>
+#include <QString>
+#include <QCryptographicHash>
+#include <QDebug>
 
 // Change to any path you wish
 static const QString path = "covid19.db";
@@ -25,7 +28,11 @@ void Registration::on_pushButton_Save_clicked()
 
     if (db.isOpen())
     {
-        db.userRegister(ui->lineEdit_email->text(),ui->lineEdit_password->text(),ui->lineEdit_lastname->text(),ui->lineEdit_firstname->text(),ui->lineEdit_phone->text(),
+        QString password = ui->lineEdit_password->text();
+        QByteArray hashed_password = QCryptographicHash::hash(password.toUtf8(),QCryptographicHash::Sha224).toHex();
+//        qDebug() << "hashed: " << hashed_password;
+
+        db.userRegister(ui->lineEdit_email->text(),hashed_password,ui->lineEdit_lastname->text(),ui->lineEdit_firstname->text(),ui->lineEdit_phone->text(),
                         ui->lineEdit_gender->text(),ui->lineEdit_nhi->text(),1,"no status",ui->lineEdit_dob->text());
 
 //        bool DbManager::userRegister(const QString& email, const QString& password, const QString& lastname, const QString& firstname,const QString& phone,
