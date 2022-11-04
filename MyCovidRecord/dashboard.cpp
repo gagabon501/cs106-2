7eaf70481078e85ababf25f88a484da3b5a73c82
Gilberto Gabon <gagabon501@yahoo.com>
2022-11-04 20:04:10 +1300

    Added icons on menu and toolbars for Dashboard

--- MyCovidRecord/dashboard.cpp
+++ MyCovidRecord/dashboard.cpp
@@ -29,6 +29,42 @@ void Dashboard::on_actionQuit_triggered()
 
 void Dashboard::on_actionViewVaccinationRecord_triggered()
 {
-    QMessageBox::information(this,"Message Title","View Vaccination Record") ;
+    QMessageBox::information(this,"Info","View Vaccination Record") ;
+}
+
+
+void Dashboard::on_actionView_RAT_Test_Record_triggered()
+{
+    QMessageBox::information(this,"Info","View RAT Test Record") ;
+}
+
+
+void Dashboard::on_actionUpload_RAT_Test_triggered()
+{
+    QMessageBox::information(this,"Info","Upload RAT Test Record") ;
+}
+
+
+void Dashboard::on_actionDownload_COVID_19_Certificate_triggered()
+{
+    QMessageBox::information(this,"Info","Download COVID-19 Vaccination Certificate") ;
+}
+
+
+void Dashboard::on_actionReport_Issues_triggered()
+{
+    QMessageBox::information(this,"Info","Report Issues") ;
+}
+
+
+void Dashboard::on_actionUpdate_Profile_triggered()
+{
+     QMessageBox::information(this,"Info","Update Profile") ;
+}
+
+
+void Dashboard::on_actionChange_Password_triggered()
+{
+     QMessageBox::information(this,"Info","Change password") ;
 }
 
--- MyCovidRecord/dashboard.h
+++ MyCovidRecord/dashboard.h
@@ -20,6 +20,18 @@ private slots:
 
     void on_actionViewVaccinationRecord_triggered();
 
+    void on_actionView_RAT_Test_Record_triggered();
+
+    void on_actionUpload_RAT_Test_triggered();
+
+    void on_actionDownload_COVID_19_Certificate_triggered();
+
+    void on_actionReport_Issues_triggered();
+
+    void on_actionUpdate_Profile_triggered();
+
+    void on_actionChange_Password_triggered();
+
 private:
     Ui::Dashboard *ui;
 };
